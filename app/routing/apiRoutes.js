b4c4bec4cdeee72852ab2a2218894b6e6120bcb1
Tami <tkapphahn@gmail.com>
2017-07-28 20:38:08 -0700

    wip

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+//***********************************
+// GET route with url /api/friends  
+// display JSON of all possible friends
+//************************************
+
+
+//**************************************
+// POST route with /api/friends
+// handles incoming survey results
+// also used to handle compatibility logic
+//*************************************
---
+++ server.js
@@ -0,0 +1,35 @@
+
+
+var express = require('express');
+app = express();
+
+var PORT = process.env.PORT || 8080;
+
+var bodyParser = require('body-parser');
+
+
+//**************************************************
+// code copied from npm body-parser
+//**************************************************
+
+// create application/json parser 
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+ 
+// parse various different custom JSON types as JSON 
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer 
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string 
+app.use(bodyParser.text({ type: 'text/html' }))
+
+// *****  end npm body-parser copied code *****************
+
+
+app.listen(PORT, function() {
+	console.log("Listening on port: " + PORT);
+});
